b6f6cdf3321bca9d5ea0c148c442696180da662b
KMB <burduninkonstantin@gmail.com>
2021-06-28 12:04:46 +0300

    get average

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,27 +5,45 @@ import org.json.simple.parser.ParseException;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class main {
     private static final String PATH = "src/main/resources/tickets.json";
+    private static final String JSON_TICKET = "tickets";
+    private static final String JSON_DEPARTURE_TIME = "departure_time";
 
     public static void main(String[] args) {
 
         JSONParser parser = new JSONParser();
-
+        List<Long> durationInMinutesList = new LinkedList<>();
         try {
             JSONObject jsonData = (JSONObject) parser.parse(getJsonFile());
-            JSONArray tickets = (JSONArray) jsonData.get("tickets");
+            JSONArray tickets = (JSONArray) jsonData.get(JSON_TICKET);
+
             tickets.forEach(t -> {
                 JSONObject ticketJsonObject = (JSONObject) t;
-                String departureTime = (String) ticketJsonObject.get("departure_time");
+                String departureTime = (String) ticketJsonObject.get(JSON_DEPARTURE_TIME);
+                String departureDate = (String) ticketJsonObject.get("departure_date");
                 String arrivalTime = (String) ticketJsonObject.get("arrival_time");
-                System.out.println("dep " + departureTime + "; arrival " + arrivalTime);
+                String arrivalDate = (String) ticketJsonObject.get("arrival_date");
+
+                LocalDateTime departureDateTime = getLocalDateTime(departureDate, departureTime);
+                LocalDateTime arrivalDateTime = getLocalDateTime(arrivalDate, arrivalTime);
+                ZoneId departureZone = ZoneId.of("Asia/Vladivostok");
+                ZoneId arrivalZone = ZoneId.of("Asia/Jerusalem");
+                Instant departureInstant = departureDateTime.atZone(departureZone).toInstant();
+                Instant arrivalInstant = arrivalDateTime.atZone(arrivalZone).toInstant();
+                durationInMinutesList.add(getDurationInMin(departureInstant, arrivalInstant));
             });
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        System.out.println("average time: "
+                + LocalTime.MIN.plus(Duration.ofMinutes(getAverageTime(durationInMinutesList))).toString());
     }
 
     private static String getJsonFile() {
@@ -40,5 +58,18 @@ public class main {
         return builder.toString().replaceAll("\\uFEFF", "");
     }
 
+    private static LocalDateTime getLocalDateTime(String date, String time) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm");
+        return LocalDateTime.parse(date + " "
+                + (time.length() == 5 ? time : "0" + time), formatter);
+    }
 
+    private static Long getDurationInMin(Instant start, Instant finish) {
+        return Duration.between(start, finish).toMinutes();
+    }
+
+    private static Long getAverageTime(List<Long> list) {
+        long sum = list.stream().mapToLong(l -> l).sum();
+        return sum / list.size();
+    }
 }
